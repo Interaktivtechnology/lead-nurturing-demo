3d1b1351987e6ccfdee5a49638ad4d36c9ecb47b
Bena <benawv@gmail.com>
2019-11-14 19:01:37 +0700

    change mcp webhook

--- donate/src/static/variable.js
+++ donate/src/static/variable.js
@@ -1,6 +1,6 @@
 
 export const API_URL = __DEV__ ? 'http://localhost:3200/' : 'https://sfdemo.interaktiv.sg/lndemo/';
-export const MCP_WEBHOOK = 'https://test.interaktiv.sg/api-step-asia/donation/confirm-payment';
+export const MCP_WEBHOOK = __DEV__ ? 'http://localhost:3200/donation/confirm-payment' : 'https://sfdemo.interaktiv.sg/lndemo/donation/confirm-payment';
 export const CMS_URL = __DEV__ ? 'http://stepasia.interaktiv.sg:1337/' : 'https://stepasia.interaktiv.sg/cms-data/';
 
 export default null;
